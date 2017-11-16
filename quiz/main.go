42c8ca746cedf39cc2a44888297ada94d16dbf67
Kosy Anyanwu <kosyfrances@gmail.com>
2017-11-16 12:34:55 +0100

    Get answer from user and return score

--- quiz/main.go
+++ quiz/main.go
@@ -6,12 +6,14 @@ import (
         "fmt"
         "log"
         "flag"
+        "bufio"
+        "strings"
 )
 
 
 func main() {
 
-    var csv_file = flag.String("file", "problems.csv", "a csv file in the format of 'question,answer'")
+    csv_file := flag.String("file", "problems.csv", "a csv file in the format of 'question,answer'")
     flag.Parse()
 
     file, err := os.Open(*csv_file)
@@ -28,6 +30,19 @@ func main() {
         log.Fatal(err)
     }
 
-    fmt.Print(quiz)
-}
+    quiz_length := len(quiz)
+    score := 0
 
+    for index, value := range quiz {
+
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Printf("Problem #%d: %s ", index, value[0])
+        answer, _ := reader.ReadString('\n')
+
+        if strings.TrimSpace(answer) == strings.TrimSpace(value[1]) {
+            score ++
+        }
+    }
+
+    fmt.Printf("You scored %d out of %d", score, quiz_length)
+}
