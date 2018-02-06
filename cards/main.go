06bd3ddba8537145183d32ca50b458cf159ec82a
Kosy Anyanwu <kosyfrances@gmail.com>
2018-02-06 08:51:33 +0100

    Create type deck and receivers

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	for i, card := rangec cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.saveToFile("my_cards")
 }
