58ff2b2ed67d0d4fb3a5f5c35b742632dfce59c8
Kosy Anyanwu <kosyfrances@gmail.com>
2019-12-05 08:55:58 +0100

    main: Create server and build handler

--- gophercises/3-choose-adventure/main.go
+++ gophercises/3-choose-adventure/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fileName := flag.String("file", "gopher.json", "the json adventure story file")
+	port := flag.Int("port", 3000, "the port to start the server for the adventure story")
 	flag.Parse()
+
 	fmt.Printf("Using the adventure story file %v", *fileName)
 
 	content, err := ioutil.ReadFile(*fileName)
@@ -21,5 +25,8 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", adv)
+	h := newHandler(adv)
+
+	fmt.Printf("Starting server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
